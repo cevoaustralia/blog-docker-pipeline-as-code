367afe0c62578e324ccd5c38aae923343a4c677b
Colin Panisset <colin.panisset@cevo.com.au>
2018-10-24 09:33:19 +1100

    Basic working service

--- utcservice.go
+++ utcservice.go
@@ -4,6 +4,9 @@ import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"net/http"
+
+	"github.com/araddon/dateparse"
+	"time"
 )
 
 func main() {
@@ -15,13 +18,43 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/", hello)
+	e.GET("/", parse)
 
 	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":8080"))
 }
 
 // Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!\n")
+func parse(c echo.Context) error {
+	input := c.QueryParam("date")
+	tz := c.QueryParam("tz")
+
+	t, err := parseDate(input, tz)
+	if err != nil {
+		return err
+	}
+	return c.String(http.StatusOK, t.String()+"\n")
+}
+
+func parseDate(input, tz string) (time.Time, error) {
+	if "" == input {
+		return time.Now(), echo.NewHTTPError(http.StatusBadRequest, "Missing query param: date")
+	}
+
+	if "" != tz {
+		loc, err := time.LoadLocation(tz)
+		if err != nil {
+			return time.Now(), echo.NewHTTPError(http.StatusBadRequest, "Invalid timezone: "+tz)
+		}
+		time.Local = loc
+	} else {
+		time.Local = time.UTC
+	}
+
+	t, err := dateparse.ParseLocal(input)
+	if err != nil {
+		return time.Now(), echo.NewHTTPError(http.StatusInternalServerError, "Could not parse date")
+	}
+
+	return t, nil
 }
